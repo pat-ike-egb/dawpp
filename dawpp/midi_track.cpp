a4b05897a3de74cf17c4c1abaf1b63e8a9980870
pat ike egb <patrick.egbuchulam@gmail.com>
2022-05-31 23:12:39 -0400

    add midi_track WIP files

---
+++ dawpp/midi_track.cpp
@@ -0,0 +1,6 @@
+//
+// Created by egbuc on 5/27/2022.
+//
+
+#include "midi_track.h"
+
---
+++ dawpp/midi_track.h
@@ -0,0 +1,17 @@
+//
+// Created by egbuc on 5/27/2022.
+//
+
+#ifndef JUCE_BASE_REPO_MIDI_TRACK_H
+#define JUCE_BASE_REPO_MIDI_TRACK_H
+
+#include "juce_audio_basics/midi/juce_MidiBuffer.h"
+
+class MidiTrack {
+private:
+    
+
+};
+
+
+#endif //JUCE_BASE_REPO_MIDI_TRACK_H
